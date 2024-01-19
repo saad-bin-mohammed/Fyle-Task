a637dadf47230c20fc56e2dc0cb25ed2cf460204
saad-bin-mohammed <94010723+saad-bin-mohammed@users.noreply.github.com>
2024-01-19 21:51:50 +0530

    rendering and pagination

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const token = "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";
+// const token = "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";
+const token = "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";
 let searchForm = document.querySelector("#searchForm");
 let header = document.querySelector("header");
+let repositories = [];
+
+const numbersArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+//name,twitter_username,login,location,html_url,company,blog,bio,avatar_url,followers,following (for User-details)
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const formData = new FormData(e.target);
@@ -11,21 +16,38 @@ searchForm.addEventListener("submit", (e) => {
       Authorization: `Bearer ${token}`,
     },
   })
-    .then((response) => response.json())
+    .then((response) => {
+      const rateLimitRemaining = response.headers.get("X-RateLimit-Remaining");
+      const rateLimitReset = response.headers.get("X-RateLimit-Reset");
+      console.log("Remaining requests:", rateLimitRemaining);
+      console.log("Reset timestamp:", rateLimitReset);
+      return response.json();
+    })
     .then((data) => {
       console.log(data);
       renderProfile(data);
-      //   name,twitter_username,login,location,html_url,company,blog,bio,avatar_url,followers,following (for Use-details)
-      //   name,html_url,description,topics[],language,visibility,forks,stargazers_count (for Repos)
+    })
+    .catch((error) => console.error("Error:", error));
+  fetch(`https://api.github.com/users/${userName}/repos`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      // renderRepos(data);
+      repositories = data;
+      renderRepos(repositories.slice(0, 10));
     })
     .catch((error) => console.error("Error:", error));
 });
 const renderProfile = (data) => {
-  document.querySelector("#profileInfo")?.remove();
+  document.querySelector("#profile")?.remove();
   header.insertAdjacentHTML(
     "afterend",
     `
-  <div id="profileInfo">
+  <div id="profile">
       <img src=${data.avatar_url} alt="" srcset="">
       <p>${data.name}</p>
       <p>${data.login}</p>
@@ -36,7 +58,72 @@ const renderProfile = (data) => {
       <p>company ${data.company}</p>
       <p>followers ${data.followers}</p>
       <p>following ${data.following}</p>
+     
   </div>
-  `
+    `
   );
+};
+const renderButtons = () => {
+  return numbersArray
+    .map(
+      (number) =>
+        `<button class="page" data-button=${number}  ${
+          number + 0 > repositories.length ? "disabled" : ""
+        }>${number + 1}</button>`
+    )
+    .join("");
+};
+//name,html_url,description,topics[],language,visibility,forks,stargazers_count (for Repos)
+const renderRepos = (data) => {
+  document.querySelector(".reposContainer")?.remove();
+  document.querySelector("#profile").insertAdjacentHTML(
+    "afterend",
+    `
+    <div class="reposContainer">
+    <div class="pages">
+    ${renderButtons()}
+    </div>
+    ${data.map(
+      (data) =>
+        `
+
+  <div class="repo">
+        <p>${data.name}</p>
+        <p>${data.description}</p>
+        <p>language ${data.language}</p>
+        <p>topics ${data.topics?.map(
+          (topic) => `
+        <p>topic ${topic}</p>`
+        )}</p>
+        <p>visibility ${data.visibility}</p>
+        <p>forks ${data.forks}</p>
+        <p>stargazers_count ${data.stargazers_count}</p>
+  </div>
+        `
+    )}
+    </div>
+    `
+  );
+  addEventListeners();
+};
+const addEventListeners = () => {
+  const buttons = document.querySelectorAll(".page");
+  buttons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+      let buttonValue = Number(
+        button.dataset.button + (button.dataset.button != "0" ? 0 : "")
+      );
+      console.log(`Button ${buttonValue} clicked`);
+      if (buttonValue < repositories.length) {
+        renderRepos(
+          repositories.slice(
+            buttonValue,
+            repositories.length > buttonValue + 10
+              ? buttonValue + 10
+              : repositories.length
+          )
+        );
+      }
+    });
+  });
 };
